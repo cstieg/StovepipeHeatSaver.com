ddb88ce9a65a83ad90e6143ad53befc6a89eced2
cstieg <cstieg4899@yahoo.com>
2017-11-08 21:45:49 -0500

    Change: Rename StovepipeHeatSaver

---
+++ StovepipeHeatSaver.com/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+using StovepipeHeatSaver.Models;
+
+namespace StovepipeHeatSaver.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected ApplicationDbContext db = new ApplicationDbContext();
+    }
+}
---
+++ StovepipeHeatSaver.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StovepipeHeatSaver.MvcApplication" Language="C#" %>
