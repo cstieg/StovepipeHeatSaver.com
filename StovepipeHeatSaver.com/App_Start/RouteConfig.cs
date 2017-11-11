ee982c3841f6418d3b5b6b1ab285bc65a927b5bc
cstieg <cstieg4899@yahoo.com>
2017-11-11 11:25:01 -0500

    Add: Custom routes

--- StovepipeHeatSaver.com/App_Start/RouteConfig.cs
+++ StovepipeHeatSaver.com/App_Start/RouteConfig.cs
@@ -1,8 +1,5 @@
 ﻿using Cstieg.ControllerHelper;
 using StovepipeHeatSaver.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -14,6 +11,8 @@ namespace StovepipeHeatSaver
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Home",
