9d7669a5bf55a906e850ce0a8e96a185921253d2
cstieg <cstieg4899@yahoo.com>
2017-11-15 22:41:46 -0500

    Add: Scroll page down to show whole video

--- StovepipeHeatSaver.com/Scripts/Site/site.js
+++ StovepipeHeatSaver.com/Scripts/Site/site.js
@@ -2,6 +2,40 @@
     $('#watch-video-tab').on('click', { toggleTarget: '#watch-video' }, toggleHidden);
 })();
 
-function toggleHidden(e) {
+async function toggleHidden(e) {
     $(e.data.toggleTarget).toggleClass('no-height');
+    if ($(e.data.toggleTarget).hasClass('no-height') == false) {
+        // sleep to wait for slide down transition
+        await sleep(1000);
+        var bottom = getCoords($(e.data.toggleTarget)[0]).top + $(e.data.toggleTarget)[0].offsetHeight;
+        var screenBottom = window.pageYOffset + window.innerHeight;
+        var scrollDown = Math.max(bottom - screenBottom, 0);
+        $('html, body').animate({
+            scrollTop: '+=' + scrollDown
+        }, 1000);
+
+    }
+}
+
+
+// stackoverflow.com
+function getCoords(elem) {
+    var box = elem.getBoundingClientRect();
+    var body = document.body;
+    var docEl = document.documentElement;
+
+    var scrollTop = window.pageYOffset;
+    var scrollLeft = window.pageXOffset;
+
+    var clientTop = docEl.clientTop || body.clientTop || 0;
+    var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+    var top = box.top + scrollTop - clientTop;
+    var left = box.left + scrollLeft - clientLeft;
+
+    return { top: Math.round(top), left: Math.round(left) };
+}
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
 }
